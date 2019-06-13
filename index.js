0824c5bf447d7bcc6cbea45b0377170005b1e2c4
John Byrne <nl03643@icloud.com>
2019-06-13 16:41:08 +0200

    updated msg again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request!🍎');
-    res.end('Hello from Azure Git!');
+    res.end('Hello from Azure Git update!');
 }
 const server = http.createServer(handler);
 server.listen (port, err => {
