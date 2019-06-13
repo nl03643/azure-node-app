5cc831b8b8dc757cc968c95fdd97650a26a3c399
John Byrne <nl03643@icloud.com>
2019-06-13 17:14:44 +0200

    test github update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request!🍎');
-    res.end('Hello from GitHub!');
+    res.end('Hello from GitHub updated!');
 }
 const server = http.createServer(handler);
 server.listen (port, err => {
