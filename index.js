ca40f24df56741822a21c74711fb39405fef9a28
John Byrne <nl03643@icloud.com>
2019-06-13 16:17:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+    console.log('Server received request!🍎');
+    res.end('Hello World!');
+}
+const server = http.createServer(handler);
+server.listen (port, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log (`Server listening on port: ${port}`);
+    }
+});
